421092afac639f84e626a7a1785c95db43a13f63
antocecere <antocecere77@gmail.com>
2020-01-06 12:21:46 +0100

    Preparing project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RoomsComponent } from './admin/rooms/rooms.component';
+import { UsersComponent } from './admin/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RoomsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
