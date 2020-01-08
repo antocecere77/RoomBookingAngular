673e9d852cfbfed7a416d2d0a7ebe5c87addbfc2
antocecere <antocecere77@gmail.com>
2020-01-08 06:50:02 +0100

    Creating the data service

--- src/app/admin/rooms/rooms.component.ts
+++ src/app/admin/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-rooms',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    console.log(this.dataService.rooms);
   }
 
 }
