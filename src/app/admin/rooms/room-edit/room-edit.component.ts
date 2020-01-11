1ad04142aa8046ee1ac92163c8911436cb6c0844
antocecere <antocecere77@gmail.com>
2020-01-11 16:31:41 +0100

    Setting and reading form control values

--- src/app/admin/rooms/room-edit/room-edit.component.ts
+++ src/app/admin/rooms/room-edit/room-edit.component.ts
@@ -22,10 +22,21 @@ export class RoomEditComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.roomForm.patchValue({
+      roomName: this.room.name,
+      location: this.room.location
+    });
   }
 
   onSubmit() {
     console.log(this.roomForm);
+
+    this.room.name = this.roomForm.controls.roomName.value;
+    this.room.location = this.roomForm.controls.location.value;
+
+    console.log(this.room);
+
+    //Call a method in the data service to save the room
   }
 
 }
